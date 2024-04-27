e038f4dbe3303f2ea22e705e2fb048364e37dec2
Alireza GoDzY <stm.stm155@gmail.com>
2024-04-27 16:51:15 +0330

    removed enforcement of env file

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     IS_SQLALCHEMY_LOG_ENABLED: bool = False
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict()
 
 
 settings = Settings()
